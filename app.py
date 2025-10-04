c2ce35c449c4646e126d735c8eb6ac39475d9828
PAGIDI KONDALA BHAVANI <bhavanipagidikondala@gmail.com>
2025-10-04 08:28:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,39 +28,39 @@ from werkzeug.wsgi import get_host
 
 import cli
 import typing as ft
-from .ctx import AppContext
-from .ctx import RequestContext
-from .globals import _cv_app
-from .globals import _cv_request
-from .globals import current_app
-from .globals import g
-from .globals import request
-from .globals import request_ctx
-from .globals import session
-from .helpers import get_debug_flag
-from .helpers import get_flashed_messages
-from .helpers import get_load_dotenv
-from .helpers import send_from_directory
-from .sansio.app import App
-from .sansio.scaffold import _sentinel
-from .sessions import SecureCookieSessionInterface
-from .sessions import SessionInterface
-from .signals import appcontext_tearing_down
-from .signals import got_request_exception
-from .signals import request_finished
-from .signals import request_started
-from .signals import request_tearing_down
-from .templating import Environment
-from .wrappers import Request
-from .wrappers import Response
+from ctx import AppContext
+from ctx import RequestContext
+from globals import _cv_app
+from globals import _cv_request
+from globals import current_app
+from globals import g
+from globals import request
+from globals import request_ctx
+from globals import session
+from helpers import get_debug_flag
+from helpers import get_flashed_messages
+from helpers import get_load_dotenv
+from helpers import send_from_directory
+from sansio.app import App
+from sansio.scaffold import _sentinel
+from sessions import SecureCookieSessionInterface
+from sessions import SessionInterface
+from signals import appcontext_tearing_down
+from signals import got_request_exception
+from signals import request_finished
+from signals import request_started
+from signals import request_tearing_down
+from templating import Environment
+from wrappers import Request
+from wrappers import Response
 
 if t.TYPE_CHECKING:  # pragma: no cover
     from _typeshed.wsgi import StartResponse
     from _typeshed.wsgi import WSGIEnvironment
 
-    from .testing import FlaskClient
-    from .testing import FlaskCliRunner
-    from .typing import HeadersValue
+    from testing import FlaskClient
+    from testing import FlaskCliRunner
+    from typing import HeadersValue
 
 T_shell_context_processor = t.TypeVar(
     "T_shell_context_processor", bound=ft.ShellContextProcessorCallable
